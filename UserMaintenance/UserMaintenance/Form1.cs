23c7a907c1fddf55e930655e6717d9f9abf8fb55
Norbert Gábor Lacz <norbert.lacz@stud.uni-corvinus.hu>
2022-09-29 21:21:11 +0200

    9feladat

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,8 @@ namespace UserMaintenance
         {
             InitializeComponent();
             label1.Text = Resource1.Fullname; 
-            button1.Text = Resource1.Add; 
+            button1.Text = Resource1.Add;
+            button2.Text = Resource1.Write;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -33,6 +35,26 @@ namespace UserMaintenance
                 FullName = textBox1.Text,
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath; 
+            sfd.Filter = "Vesszővel tagolt szöveg (*.csv) | *.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8);
+                foreach (User u in users) 
+                {
+                    sw.WriteLine($"{u.ID};{u.FullName}");
+                }
+                sw.Close();
+
+            }
         }
     }
 }
