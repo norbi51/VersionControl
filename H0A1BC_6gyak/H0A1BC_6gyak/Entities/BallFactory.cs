0058e84a367687b67c9a43536e99b320d77499c2
h0a1bc <h0a1bc@SZA0032048>
2022-11-04 12:09:29 +0100

    2. feladat

---
+++ H0A1BC_6gyak/H0A1BC_6gyak/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H0A1BC_6gyak.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
