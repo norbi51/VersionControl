6bbe6b18a40dce669efbfdc44253a9fddac8e4d7
h0a1bc <h0a1bc@SZA0032048>
2022-11-11 12:15:35 +0100

    6feladatig

--- H0A1BC_7gyak/H0A1BC_7gyak/Form1.cs
+++ H0A1BC_7gyak/H0A1BC_7gyak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using H0A1BC_7gyak.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,27 @@ namespace H0A1BC_7gyak
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
