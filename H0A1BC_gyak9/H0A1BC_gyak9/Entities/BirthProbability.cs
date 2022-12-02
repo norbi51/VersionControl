418bb5e209109ea340766442435228f6ac8d2484
h0a1bc <h0a1bc@SZA0032048>
2022-12-02 11:56:29 +0100

    1. feladat

---
+++ H0A1BC_gyak9/H0A1BC_gyak9/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H0A1BC_gyak9.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+
+        public double Probability { get; set; }
+    }
+}
---
+++ H0A1BC_gyak9/H0A1BC_gyak9/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H0A1BC_gyak9.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double Probability { get; set; }
+
+    }
+}
